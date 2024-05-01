0a57f2e0401c602e049c2a5f5ab4c8ae5b43c839
UditNayak <uditnayak027@gmail.com>
2024-05-01 15:55:29 +0530

    first commit

---
+++ src/main/java/udit/dev/schooltimetablemanagementsystem/SchoolTimeTableManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package udit.dev.schooltimetablemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolTimeTableManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolTimeTableManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/udit/dev/schooltimetablemanagementsystem/SchoolTimeTableManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package udit.dev.schooltimetablemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolTimeTableManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
